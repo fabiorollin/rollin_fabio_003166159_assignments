06e20534a0c035cb6abb08255592f6f2cd9a68f4
fabiorollin <fabiorollin@gmail.com>
2026-02-14 16:21:50 -0500

    work Aerea

--- Module6ProfileWorkarea/src/UserInterface/WorkAreas/FacultyRole/FacultyWorkAreaJPanel.java
+++ Module6ProfileWorkarea/src/UserInterface/WorkAreas/FacultyRole/FacultyWorkAreaJPanel.java
@@ -149,13 +149,7 @@ public class FacultyWorkAreaJPanel extends javax.swing.JPanel {
 
     private void jButton4IdentifyResourceAssetsActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4IdentifyResourceAssetsActionPerformed
         // TODO add your handling code here:
-        CardSequencePanel.removeAll();
-
-        ManagePersonsJPanel aos = new ManagePersonsJPanel(business, CardSequencePanel);
-
-        CardSequencePanel.add("Manage Persons", aos);
-        ((java.awt.CardLayout) CardSequencePanel.getLayout()).next(CardSequencePanel);
-
+        
     }//GEN-LAST:event_jButton4IdentifyResourceAssetsActionPerformed
 
     private void jButton10ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton10ActionPerformed
