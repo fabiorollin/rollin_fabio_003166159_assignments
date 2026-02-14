48806d704bc1752fd6b69007c5ba70b8fbeecc23
fabiorollin <fabiorollin@gmail.com>
2026-02-14 15:39:19 -0500

    Faculty profile and student area

--- Module6ProfileWorkarea/src/Business/ProfileWorkAreaMainFrame.java
+++ Module6ProfileWorkarea/src/Business/ProfileWorkAreaMainFrame.java
@@ -11,6 +11,7 @@ import Business.Profiles.StudentProfile;
 
 import Business.UserAccounts.UserAccount;
 import Business.UserAccounts.UserAccountDirectory;
+import Business.Profiles.FacultyProfile;
 
 import UserInterface.WorkAreas.AdminRole.AdminRoleWorkAreaJPanel;
 import UserInterface.WorkAreas.FacultyRole.FacultyWorkAreaJPanel;
@@ -183,14 +184,14 @@ public class ProfileWorkAreaMainFrame extends javax.swing.JFrame {
 
         }
 
- /*      if (profile instanceof FacultyProfile) {
+         if (profile instanceof FacultyProfile) {
             facultyworkarea = new FacultyWorkAreaJPanel(business, CardSequencePanel);
             CardSequencePanel.removeAll();
             CardSequencePanel.add("faculty", facultyworkarea);
             ((java.awt.CardLayout) CardSequencePanel.getLayout()).next(CardSequencePanel);
 
         }
-*/
+
 
     }//GEN-LAST:event_LoginButtonActionPerformed
 
